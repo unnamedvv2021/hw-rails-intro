ae81719448072c8bb4a23a3747089f151ac2dca5
EC2 Default User <ec2-user@ip-172-31-12-115.ec2.internal>
2022-02-28 02:10:22 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,6 @@
 class MoviesController < ApplicationController
 
     def show
-      session.clear
       id = params[:id] # retrieve movie ID from URI route
       @movie = Movie.find(id) # look up movie by unique ID
       # will render app/views/movies/show.<extension> by default
